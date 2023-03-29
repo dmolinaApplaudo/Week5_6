f0b0c85fc0031f1457617e061a2fbf5366766572
dmolina <dmolina@applaudostudios.dev>
2023-03-29 10:58:57 -0600

    Changed Implementation of user Service, mistook previous dependency.

--- src/main/java/com/week5/homework/service/impl/UserServiceImpl.java
+++ src/main/java/com/week5/homework/service/impl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.week5.homework.service.impl;
 
 import com.week5.homework.persistence.model.User;
+import com.week5.homework.persistence.repository.IUserRepository;
 import com.week5.homework.service.IUserService;
 import org.springframework.stereotype.Service;
 
@@ -9,14 +10,14 @@ import java.util.Optional;
 @Service
 public class UserServiceImpl implements IUserService {
 
-    private IUserService userService;
+    private IUserRepository userRepository;
 
-    public UserServiceImpl(IUserService userService) {
-        this.userService = userService;
+    public UserServiceImpl(IUserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
     @Override
     public Optional<User> findByEmail(String email) {
-        return Optional.empty();
+        return userRepository.findById(email);
     }
 }
