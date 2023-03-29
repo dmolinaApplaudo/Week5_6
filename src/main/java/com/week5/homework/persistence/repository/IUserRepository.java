d2ed3dcac05f132904882e36917d84b05accc2fb
dmolina <dmolina@applaudostudios.dev>
2023-03-29 10:46:56 -0600

    Forgot to add notation to repository Interface

--- src/main/java/com/week5/homework/persistence/repository/IUserRepository.java
+++ src/main/java/com/week5/homework/persistence/repository/IUserRepository.java
@@ -3,6 +3,8 @@ package com.week5.homework.persistence.repository;
 import com.week5.homework.persistence.model.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IUserRepository extends PagingAndSortingRepository<User,String>, CrudRepository<User,String> {
 }
