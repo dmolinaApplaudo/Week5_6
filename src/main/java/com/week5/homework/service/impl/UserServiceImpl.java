957034cc61f31f61bccbf4d6085d535a4ff2a293
dmolina <dmolina@applaudostudios.dev>
2023-03-29 10:56:11 -0600

    Added miminal immplementation to UserServiceImpl to start testing the Validations before adding New Users.

--- src/main/java/com/week5/homework/service/impl/UserServiceImpl.java
+++ src/main/java/com/week5/homework/service/impl/UserServiceImpl.java
@@ -1,4 +1,22 @@
 package com.week5.homework.service.impl;
 
-public class UserServiceImpl {
+import com.week5.homework.persistence.model.User;
+import com.week5.homework.service.IUserService;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements IUserService {
+
+    private IUserService userService;
+
+    public UserServiceImpl(IUserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        return Optional.empty();
+    }
 }
