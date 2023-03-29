1e51a66b46d413e2d33b612684b8ed8629bc6929
dmolina <dmolina@applaudostudios.dev>
2023-03-29 10:53:11 -0600

    Added first Function to Test Immplementation of Adding Users.

--- src/main/java/com/week5/homework/service/IUserService.java
+++ src/main/java/com/week5/homework/service/IUserService.java
@@ -1,4 +1,9 @@
 package com.week5.homework.service;
 
+import com.week5.homework.persistence.model.User;
+
+import java.util.Optional;
+
 public interface IUserService {
+    Optional<User> findByEmail(String email);
 }
